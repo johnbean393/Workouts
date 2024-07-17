a46a473d3f13ab5db762176986c30d15b215752f
johnbean393 <johnbean393@gmail.com>
2024-07-17 23:35:28 +0800

    Initial Commit

---
+++ Workout Timer/Workout_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TimerApp.swift
+//  Workout Timer
+//
+//  Created by Bean John on 17/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
